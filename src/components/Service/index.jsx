fa2f68a58a908588a00d6cec2c7e6ace41d353f9
Ruqayah1204 <mhiedey98@gmail.com>
2024-03-30 15:46:40 +0100

    updated

---
+++ src/components/Service/index.jsx
@@ -0,0 +1,16 @@
+
+import Card from "../Card";
+import styles from './service.module.css';
+
+
+
+const Service = () => {
+    return <div className={styles.container} >
+        <h2>Our Service</h2>
+        <div>
+                <Card  />      
+        </div>
+    </div>
+}
+
+export default Service;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,14 @@
 import Footer from "../../components/Footer";
 import Hero from "../../components/Hero";
+import Service from "../../components/Service";
+
 
 const Home = () => {
     return <>
         <Hero />
+        <Service />
         <Footer/>
+        
     </>
 }
 
